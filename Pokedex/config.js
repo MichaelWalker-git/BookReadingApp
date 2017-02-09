c4a69bc05e9359f381f0cb949487a5c6590ac580
Michael Tran <AtlasCoughed@users.noreply.github.com>
2017-02-08 20:26:35 -0800

    [feature] - starting up files for pokebot

---
+++ Pokedex/config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by miketran on 2/8/17.
+ */
+
+const config = {
+    appId: 'YOUR APPID',
+    appPassword: 'YOUR APP PASSWORD',
+}
+module.exports = config
---
+++ Pokedex/pokebot.js
@@ -0,0 +1,7 @@
+/**
+ * Created by miketran on 2/8/17.
+ */
+
+const restify = require('restify')
+const builder = require('botbuilder')
+const server = restify.createServer().listen(8080)
